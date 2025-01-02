35f297b8f712a9813b8915587bf19c13f371bdf6
Sk93804 <sudheesh.zx@gmail.com>
2025-01-02 12:30:09 +0530

    Update

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,7 @@
-def call(String job = env.JOB_NAME) {
+def call(String goal, String name, String job = env.JOB_NAME) {
     if(!job){
         error "Job name is not provided"
     }
-    sh "mvn clean package -Djob=${job}" 
+    sh "mvn ${goal} ${name} -Djob=${job}" 
     echo "${env.JOB_NAME}"
 }
