f414ab24c447c78087012cef2762de11df968fe5
Sk93804 <sudheesh.zx@gmail.com>
2025-01-03 09:53:43 +0530

    Update

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,4 +1,4 @@
 def call(Map config){
      echo "Code checkout in ${config.env}"
-     git branch: '${config.branch}', url: '${config.url}'
-}
+     git branch: "${config.branch}", url: "${config.url}"
+     }
