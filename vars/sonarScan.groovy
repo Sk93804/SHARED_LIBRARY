5b8b5375878ab99a8145eb769cb721ac2d42295f
Sk93804 <sudheesh.zx@gmail.com>
2025-05-05 13:26:49 +0530

    Update

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,6 +1,6 @@
 def call(){
     withSonarQubeEnv(config.SONARQUBE_ENV){
-        sh """ sonar:sonar \
+        sh """ mvn sonar:sonar \
         -Dsonar.projectKey=${config.projectKey} 
         -Dsonar.host.url=${config.sonarUrl}"""
     }
