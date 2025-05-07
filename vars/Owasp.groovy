8815c9da2fc58cdb72029948a60d8a658fb42be9
Sk93804 <sudheesh.zx@gmail.com>
2025-05-07 16:43:48 +0530

    Update

--- vars/Owasp.groovy
+++ vars/Owasp.groovy
@@ -1,6 +1,6 @@
 def call() {
     // Ensure Dependency-Check is run in the correct workspace directory
     dir('./jenkins/workspace/First_job') {
-        sh 'dependency-check --scan . --format HTML --out ./dependency-check-report --nvdApiKey 37e63e4f-2cde-4ac7-923d-c7e17fc2d30c'
+        sh 'dependency-check --data ./dc-cache --scan . --format HTML --out ./dependency-check-report --nvdApiKey 37e63e4f-2cde-4ac7-923d-c7e17fc2d30c'
     }
 }
